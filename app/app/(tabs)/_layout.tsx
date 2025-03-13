a8f1712fb26753140fbe98db4a633e382988b5d6
Timothy Lidede <lidedetimothy@gmail.com>
2025-03-13 18:48:55 +0300

    explore source file now named scan

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -37,7 +37,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="scan"
         options={{
           title: "Scan",
           tabBarIcon: ({ color }) => (
